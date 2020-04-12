ff8b4d27bb5a555a4f139527b238dd1acae4171f
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-12 13:21:39 +0700

    fix layouting history and ticket

--- src/screens/History.js
+++ src/screens/History.js
@@ -1,15 +1,45 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {Header} from 'react-native-elements';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {Header, Card} from 'react-native-elements';
+
+const users = [
+  {
+    name: 'Efisiensi',
+    seat: '5A',
+    departure: 'Semarang',
+    destination: 'Surabaya',
+    timeGo: '04:00:00 WIB',
+    arrive: '06:00:00 WIB',
+    date: '24-04-2020',
+  },
+];
+
+const localStyles = StyleSheet.create({
+  user: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 10,
+    borderWidth: 1,
+    borderBottomColor: '#e8e8e8',
+    borderTopColor: 'white',
+    borderRightColor: 'white',
+    borderLeftColor: 'white',
+  },
+  name: {},
+  image: {},
+});
 
 class History extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <View>
         <View>
           <Header
-            leftComponent={{
-              text: 'BusGo',
+            centerComponent={{
+              text: 'Your Trips',
               style: {color: 'blue', fontSize: 25},
             }}
             containerStyle={{
@@ -19,7 +49,38 @@ class History extends Component {
             }}
           />
         </View>
-        <Text>Recent Trips</Text>
+        <Card title="PO. Efisiensi">
+          {users.map((u, i) => {
+            return (
+              <View style={localStyles.cardView}>
+                <View key={i} style={localStyles.user}>
+                  <View>
+                    <Text style={localStyles.name}>{u.name}</Text>
+                    <Text> {u.seat} </Text>
+                  </View>
+                  <View>
+                    <Text>{u.departure}</Text>
+                    <Text> {u.destination} </Text>
+                  </View>
+                  <View>
+                    <Text> {u.timeGo} </Text>
+                    <Text> {u.arrive} </Text>
+                  </View>
+                  <Text> {u.date} </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingTop: 5,
+                  }}>
+                  <Text>Total : Rp.25.000</Text>
+                  <Text style={{color: 'orange'}}>Detail</Text>
+                </View>
+              </View>
+            );
+          })}
+        </Card>
       </View>
     );
   }
--- src/screens/Ticket.js
+++ src/screens/Ticket.js
@@ -1,6 +1,38 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {Header} from 'react-native-elements';
+import {View, Text, StyleSheet} from 'react-native';
+import {Header, Card} from 'react-native-elements';
+
+const users = [
+  {
+    date: '26 April 2020',
+    day: 'Sunday',
+    name: 'Ayana',
+    email: 'ayana66@gmail.com',
+    departure: 'Semarang',
+    destination: 'Surabaya',
+    timeGo: '04:00:00 WIB',
+    arrive: '06:00:00 WIB',
+    seat: '39A',
+    Code: 'TEW45g',
+    Price: 'Rp 25.000,00',
+    status: 'Booked',
+  },
+];
+
+const localStyles = StyleSheet.create({
+  user: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 10,
+    borderWidth: 1,
+    borderBottomColor: 'black',
+    borderTopColor: 'white',
+    borderRightColor: 'white',
+    borderLeftColor: 'white',
+  },
+  name: {},
+  image: {},
+});
 
 class Ticket extends Component {
   render() {
@@ -8,8 +40,8 @@ class Ticket extends Component {
       <View>
         <View>
           <Header
-            leftComponent={{
-              text: 'BusGo',
+            centerComponent={{
+              text: 'Your Ticket Detail',
               style: {color: 'blue', fontSize: 25},
             }}
             containerStyle={{
@@ -19,7 +51,104 @@ class Ticket extends Component {
             }}
           />
         </View>
-        <Text>This is Ticket Screen</Text>
+        <Card>
+          {users.map((u, i) => {
+            return (
+              <View>
+                <View key={i} style={{backgroundColor: '#ffd245'}}>
+                  <View style={localStyles.user}>
+                    <View>
+                      <Text style={{fontWeight: 'bold'}}>{u.date}</Text>
+                      <Text style={{fontWeight: 'bold'}}> {u.day} </Text>
+                    </View>
+                    <View>
+                      <Text style={{fontWeight: 'bold'}}>{u.name}</Text>
+                      <Text style={{fontWeight: 'bold'}}> {u.email} </Text>
+                    </View>
+                  </View>
+                  <View>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        padding: 10,
+                      }}>
+                      <Text style={{fontWeight: 'bold'}}> {u.timeGo} </Text>
+                      <Text style={{fontWeight: 'bold'}}> {u.arrive} </Text>
+                    </View>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        borderWidth: 1,
+                        borderTopColor: 'black',
+                        borderLeftColor: 'white',
+                        borderRightColor: 'white',
+                        borderBottomColor: 'black',
+                        padding: 10,
+                      }}>
+                      <View>
+                        <Text style={{fontWeight: 'bold'}}>Departure</Text>
+                        <Text style={{fontWeight: 'bold'}}>Destination</Text>
+                      </View>
+                      <View>
+                        <Text style={{fontWeight: 'bold'}}>
+                          {' '}
+                          {u.departure}{' '}
+                        </Text>
+                        <Text style={{fontWeight: 'bold'}}>
+                          {' '}
+                          {u.destination}{' '}
+                        </Text>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+                <View style={{backgroundColor: '#e8e8e8'}}>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      padding: 5,
+                    }}>
+                    <Text style={{opacity: 0.5}}>Seat No :</Text>
+                    <Text> {u.seat} </Text>
+                  </View>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      padding: 5,
+                    }}>
+                    <Text style={{opacity: 0.5}}>Code:</Text>
+                    <Text> {u.Code} </Text>
+                  </View>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      padding: 5,
+                    }}>
+                    <Text style={{opacity: 0.5}}>Price:</Text>
+                    <Text> {u.Price} </Text>
+                  </View>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      padding: 5,
+                    }}>
+                    <Text>Status:</Text>
+                    <Text style={{color: 'orange', fontWeight: 'bold'}}>
+                      {' '}
+                      {u.status}{' '}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            );
+          })}
+        </Card>
       </View>
     );
   }
